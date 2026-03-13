36a9c4c20f0adf60b294a8950b0c744486843bf9
olgafirenze <olgafirenze@gmail.com>
2026-03-13 13:22:04 +0300

    Домашнее задание 3

---
+++ src/main/java/lesson3/Park.java
@@ -0,0 +1,94 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Park {
+    private String parkName;
+    private Address address;
+    private LocalTime parkOpenTime;
+    private LocalTime parkCloseTime;
+    private ArrayList<Attraction> attractions = new ArrayList<>();
+
+    public class Address {
+        private String city;
+        private String street;
+        private int building;
+
+        public Address(String city, String street, int building) {
+            this.city = city;
+            this.street = street;
+            this.building = building;
+        }
+
+        public String getFullAddress() {
+            return city + ", " + street + ", д. " + building;
+        }
+    }
+
+    public class Attraction {
+        private String attractionName;
+        private LocalTime attractionOpenTime;
+        private LocalTime attractionCloseTime;
+        private double price;
+
+        public Attraction(String attractionName, LocalTime attractionOpenTime, LocalTime attractionCloseTime, double price) {
+            this.attractionName = attractionName;
+            this.attractionOpenTime = attractionOpenTime;
+            this.attractionCloseTime = attractionOpenTime;
+            this.price = price;
+        }
+
+        public String getInfo() {
+            return attractionName + " " + attractionOpenTime + " - " + attractionCloseTime + " " + price + " ₽";
+        }
+    }
+
+
+    public Park(String parkName, String city, String street, int building,
+                LocalTime parkOpenTime, LocalTime parkCloseTime) {
+        this.parkName = parkName;
+        this.parkOpenTime = parkOpenTime;
+        this.parkCloseTime = parkCloseTime;
+        this.address = this.new Address(city, street, building);
+    }
+
+    public void addAttraction(String attractionName, LocalTime attractionOpenTime, LocalTime attractionCloseTime, double price) {
+        Attraction attraction = this.new Attraction(attractionName, attractionOpenTime, attractionOpenTime, price);
+        attractions.add(attraction);
+    }
+
+    public void printParkInfo() {
+        System.out.println(parkName);
+        System.out.println("Адрес: " + address.getFullAddress());
+        System.out.println("Время работы: " + parkOpenTime + " - " + parkCloseTime);
+        System.out.println("Аттракционы:");
+        for (Attraction a : attractions) {
+            System.out.println(a.getInfo());
+        }
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        Park park = new Park(
+                "Парк Горького",
+                "Москва", "ул. Крымский Вал", 9,
+                LocalTime.parse("09:00", formatter),
+                LocalTime.parse("23:00", formatter)
+        );
+
+        park.addAttraction("Колесо обозрения",
+                LocalTime.parse("10:00", formatter),
+                LocalTime.parse("22:00", formatter), 300);
+
+        park.addAttraction("Американские горки",
+                LocalTime.parse("11:00", formatter),
+                LocalTime.parse("21:00", formatter), 500);
+
+        park.addAttraction("Комната страха",
+                LocalTime.parse("12:00", formatter),
+                LocalTime.parse("20:00", formatter), 400);
+
+        park.printParkInfo();
+    }
+}
---
+++ src/main/java/lesson3/Product.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Product {
+    private String name;
+    private LocalDate prodDate;
+    private String manufacturer;
+    private String originCountry;
+    private double price;
+    private boolean reserved;
+
+    public Product(String name, LocalDate prodDate, String manufacturer, String originCountry, double price, boolean reserved) {
+        this.name = name;
+        this.prodDate = prodDate;
+        this.manufacturer = manufacturer;
+        this.originCountry = originCountry;
+        this.price = price;
+        this.reserved = reserved;
+    }
+
+    public void info() {
+        System.out.println("название: " + name + "; дата производства: " + prodDate + "; производитель: " + manufacturer + "; страна происхождения: " + originCountry + "; цена: " + price + "; состояние бронирования покупателем: " + reserved);
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        Product[] productsArray = new Product[5];
+        productsArray[0] = new Product("Samsung S25 Ultra", LocalDate.parse("01.02.2025", dateFormatter), "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("X1", LocalDate.parse("02.03.2025", dateFormatter), "Autovaz", "Russia", 15000, false);
+        productsArray[2] = new Product("Y 2", LocalDate.parse("03.04.2025", dateFormatter), "Amazon", "USA", 15599, false);
+        productsArray[3] = new Product("123 Canada", LocalDate.parse("04.05.2025", dateFormatter), "Samsung Corp.", "Canada", 12599, true);
+        productsArray[4] = new Product("Yamaha 500", LocalDate.parse("05.06.2025", dateFormatter), "Yamaha Corp.", "Japan", 10000, true);
+        for (Product product : productsArray) {
+            product.info();
+        }
+    }
+}
